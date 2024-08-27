32027c091c145106bfde1b13bc80b5ae98103dfd
NGUYEN-XUAN-THUY <thuynguyen27102005@gmail.com>
2024-08-27 10:59:19 +0700

    add lan 3

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
 const btn = document.querySelectorAll(".btn");
+const food = document.querySelectorAll(".food-item");
+
 function removeActive() {
   btn.forEach((even) => {
     even.classList.remove("active");
   });
 }
+
+function filter(clickedButton) {
+  food.forEach((even) => {
+    if (
+      even.getAttribute("type-food") ===
+        clickedButton.getAttribute("type-food") ||
+      clickedButton.getAttribute("type-food") === "all"
+    ) {
+      even.classList.remove("hide");
+    } else {
+      even.classList.add("hide");
+    }
+  });
+}
+
 btn.forEach((item) => {
   item.addEventListener("click", () => {
     removeActive();
     item.classList.add("active");
+    filter(item);
   });
 });
